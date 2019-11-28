73fb122058f845b1b3c32b7b19abe2fcb89a4eaf
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2019-11-28 22:56:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-console.log('Hello World from Node.js');
+
 app.get('/',function(req,res) {
   res.send("Hello World my name is Sanjib Behera");
 });
